857cdae6ef26366282f46ad29a1046efa742b267
Kingaa6 <Kingaa6@gmail.com>
2019-03-06 12:48:50 -0800

    Add test for buttons rendering with proper enable/disable

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -7,4 +7,28 @@ describe('<Controls />', () => {
   it('renders without failing', () => {
     render(<Controls />);
   });
+
+  // check if controls render
+
+  describe('buttons', () => {
+    it('lock button renders with default disabled', () => {
+      const { getByText } = render(<Controls />);
+
+      const button = getByText(/lock gate/i);
+
+      expect(button.textContent).toBe('Lock Gate');
+      expect(button.disabled).toBeTruthy();
+    });
+  });
+
+  describe('buttons', () => {
+    it('close button renders with default enabled', () => {
+      const { getByText } = render(<Controls />);
+
+      const button = getByText(/close gate/i);
+
+      expect(button.textContent).toBe('Close Gate');
+      expect(button.disabled).toBeFalsy();
+    });
+  });
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,4 +7,22 @@ describe('<Dashboard />', () => {
   it('renders without failing', () => {
     render(<Dashboard />);
   });
+
+  // check initial state of lock gate and close gate
+
+  // button has disable property on it. ie disable=true
+
+  // test on dashboard ?
+  // check if lock button fires
+  // check that lock button can only fire when gate is open and unlocked
+
+  // check if close button fires
+  // check that close button can only fire when gate is open and unlocked
 });
+
+// tips
+// - check which classes
+// - check for disabled or enabled
+// - check for default props
+// - check when class is locked / unlocked that text rendered is correct
+// - write 5-10 tests per component for MVP
